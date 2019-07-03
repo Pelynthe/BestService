440d05eb9a4a588a75bcaf229502df9255d27c29
Pelynthe <sigma.no.gemini@gmail.com>
2019-07-03 22:57:21 +0300

    server->web-service

--- eventprocessor/web-service/src/main/java/ru/sberbank/school/web/Main.java
+++ eventprocessor/web-service/src/main/java/ru/sberbank/school/web/Main.java
@@ -1,4 +1,4 @@
-package ru.sberbank.school;
+package ru.sberbank.school.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
