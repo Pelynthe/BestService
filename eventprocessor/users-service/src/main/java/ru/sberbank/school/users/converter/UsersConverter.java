48cb1e4e9e1aa89541ee23ffb9cbb93599982c1c
Pelynthe <sigma.no.gemini@gmail.com>
2019-07-21 20:39:09 +0300

    Правки liquibase для remotemysql

--- eventprocessor/users-service/src/main/java/ru/sberbank/school/users/converter/UsersConverter.java
+++ eventprocessor/users-service/src/main/java/ru/sberbank/school/users/converter/UsersConverter.java
@@ -26,6 +26,7 @@ public class UsersConverter {
                 .email(user.getEmail())
                 .firstName(user.getFirstName())
                 .lastName(user.getLastName())
+                .password(user.getPassword())
                 .build();
     }
 }
