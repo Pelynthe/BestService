b0a81a7a245e952b9ada882815176996f928a844
Pelynthe <sigma.no.gemini@gmail.com>
2019-07-23 00:19:31 +0300

    user-service: roles in model

--- eventprocessor/users-client/src/main/java/ru/sberbank/school/feign/model/UserModel.java
+++ eventprocessor/users-client/src/main/java/ru/sberbank/school/feign/model/UserModel.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -17,4 +19,5 @@ public class UserModel {
 	String firstName;
 	String lastName;
 	String password;
+	List<String> roles;
 }
